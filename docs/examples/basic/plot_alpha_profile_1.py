646ec7e2040b99a8c5e09717434807bac703a867
MartinPdeS <Martin.Poinsinet.de.Sivry@gmail.com>
2023-12-07 13:31:37 -0500

    version bump [publish]

---
+++ docs/examples/basic/plot_alpha_profile_1.py
@@ -0,0 +1,31 @@
+"""
+Symmetric coupler z-profile
+===========================
+"""
+
+# %%
+# Importing the script dependencies
+from SuPyMode.workflow import AlphaProfile
+
+
+profile = AlphaProfile(symmetric=True, add_end_of_taper_section=True)
+
+# %%
+# Adding a first taper segment with large initial heating length (i.e. slow reduction)
+profile.add_taper_segment(
+    alpha=0,
+    initial_heating_length=8e-3,
+    stretching_length=0.2e-3 * 20
+)
+
+# %%
+# Adding a first taper segment with small initial heating length (i.e. steep reduction)
+profile.add_taper_segment(
+    alpha=0,
+    initial_heating_length=2e-3,
+    stretching_length=0.2e-3 * 20
+)
+
+profile.plot().show()
+
+# -
