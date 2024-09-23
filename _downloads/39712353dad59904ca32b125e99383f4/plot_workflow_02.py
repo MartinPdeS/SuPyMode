540af406e0644cefe94631bd0da1b26972e89703
MartinPdeS <MartinPdeS@users.noreply.github.com>
2024-09-23 22:32:03 +0000

    deploy: 739c17e10c592e71e864dfeed41eff66b364d1cd

---
+++ _downloads/39712353dad59904ca32b125e99383f4/plot_workflow_02.py
@@ -0,0 +1,77 @@
+"""
+2x2 Coupler
+===========
+"""
+
+
+# %%
+# Importing the script dependencies
+from SuPyMode.workflow import Workflow, configuration, fiber_catalogue, Boundaries
+
+wavelength = 1550e-9
+
+# %%
+# Generating the fiber structure
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Here we define the cladding and fiber structure to model the problem
+clad_structure = configuration.ring.FusedProfile_02x02
+
+fiber_list = [
+    fiber_catalogue.load_fiber('DCF1300S_33', wavelength=wavelength),
+    fiber_catalogue.load_fiber('DCF1300S_33', wavelength=wavelength),
+]
+
+# %%
+# Defining the boundaries of the system
+boundaries = [
+    Boundaries(right='symmetric', top='symmetric'),
+    Boundaries(right='symmetric', top='anti-symmetric')
+]
+
+# %%
+# Generating the computing workflow
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Workflow class to define all the computation parameters before initializing the solver
+workflow = Workflow(
+    fiber_list=fiber_list,          # List of fiber to be added in the mesh, the order matters.
+    clad_structure=clad_structure,  # Cladding structure, if None provided then no cladding is set.
+    fusion_degree=0.9,              # Degree of fusion of the structure if applicable.
+    wavelength=wavelength,          # Wavelength used for the mode computation.
+    resolution=50,                  # Number of point in the x and y axis [is divided by half if symmetric or anti-symmetric boundaries].
+    x_bounds="left",                # Mesh x-boundary structure.
+    y_bounds="bottom",              # Mesh y-boundary structure.
+    boundaries=boundaries,          # Set of symmetries to be evaluated, each symmetry add a round of simulation
+    n_sorted_mode=4,                # Total computed and sorted mode.
+    n_added_mode=2,                 # Additional computed mode that are not considered later except for field comparison [the higher the better but the slower].
+    plot_geometry=True,             # Plot the geometry mesh before computation.
+    debug_mode=0,                   # Print the iteration step for the solver plus some other important steps.
+    auto_label=True,                # Auto labeling the mode. Label are not always correct and should be verified afterwards.
+    itr_final=0.1,                  # Final value of inverse taper ratio to simulate
+    clad_rotation=0,                # Rotate the geoemtry in the given angle in degree
+    index_scrambling=1e-4           # Scrambling of refractive index value in order to lift mode degeneracy [useful for some analysis]
+)
+
+superset = workflow.get_superset()
+
+# %%
+# Field computation: :math:`E_{i,j}`
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+_ = superset.plot(plot_type='field', itr_list=[1.0, 0.1])
+
+# %%
+# Effective index: :math:`n^{eff}_{i,j}`
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+_ = superset.plot(plot_type='index')
+
+# %%
+# Modal normalized coupling: :math:`C_{i,j}`
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+_ = superset.plot(plot_type='normalized-coupling')
+
+# %%
+# Adiabatic criterion: :math:`\tilde{C}_{i,j}`
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+_ = superset.plot(plot_type='adiabatic')
+
+
+# -
